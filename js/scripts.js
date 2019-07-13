a8e8dd110a6416558d075776c6805ef804e8c800
Joseck Osugo <josugo38@gmail.com>
2019-07-13 23:29:22 +0300

    add simple javascript functions

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+var a = [];
+var b = [];
+var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+var femaleNames = ["Akosua","Adwoa","Abenaa","Aku","Yaa","Afua","Ama"];
+var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
+function century(){
+  var cc = parseInt(document.getElementById("year").value);
+  a.push(cc);
+}
+function year (){
+  var yy = parseInt(document.getElementById("year").value);
+  a.push(yy);
+}
+function month(){
+  var mm =parse
+}
