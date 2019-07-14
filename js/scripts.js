2dae81d26ef4c59a7da7cc4a2b28f133d3d3ab63
Joseck Osugo <josugo38@gmail.com>
2019-07-14 15:13:26 +0300

    add  correct name to user

--- js/scripts.js
+++ js/scripts.js
@@ -10,17 +10,94 @@ function determine(){
  var formula = (((cc/4)-2*cc-1)+((5*yy/4))+((26*(mm+1)/10))+dd)%7;
  var result= formula.toFixed();
 
-if (result = 2 && gender===1){
-   alert("You were born on Tuesday and your name would be Yaw");
+ if (result===0 && gender===1){
+       alert("You were born on a SUNDAY  and your name would be KWASI");
+       return true;
+   }
+   else if (result===1 && gender===1) {
+     alert("You were born on a MONDAY and your name would be KWADWO");
+     return true;
+   }
+   else if (result===2 && gender===1) {
+     alert("You were born on a TUESDAY and your name would be KWABENA");
+     return true;
+   }
+   else if (result===3 && gender===1) {
+     alert("You were born on a WEDNESDAY and your name would be  KWAKU");
+     return true;
+   }
+   else if (result===4 && gender===1) {
+     alert("You were born on a THURSDAY and your name would be YAW");
+     return true;
+   }
+   else if (result===5 && gender===1) {
+     alert("You were born on a FRIDAY and your name would be KOFI");
+     return true;
+   }
+   else if (result===6 && gender===1) {
+     alert("You were born on a SATURDAY and your name would be KWAME ");
+     return true;
+   }
+   else if (result===0 && gender===2) {
+     alert("You were born on a SUNDAY and your name would be AKOSUA");
+     return true;
+   }
+   else if (result===1 && gender===2) {
+     alert("You were born on a MONDAY and your name would be ADWOA");
+     return true;
+   }
+   else if (result===2 && gender===2) {
+     alert("You were born on a TUESDAY and your name would be ABENAA");
+     return true;
+   }
+   else if (result===3 && gender===2) {
+    alert("You were born on a WEDNESDAY and your name would be AKU");
+    return true;
+   }
+   else if (result===4 && gender===2) {
+     alert("You were born on a THURSDAY and your name would be YAA");
+     return true;
+   }
+   else if (result===5 && gender===2) {
+     alert("You were born on a FRIDAY and your name would be AFUA");
+     return true;
+   }
+   else{
+     alert("You were born on a SATURDAY and your name would be AMA");
+     return true;
+   }
  }
- else {
-   document.write(result);
- }
 
 
 
 
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+ /*
+ if (result = 2 && gender===1){
+    alert("You were born on Tuesday and your name would be Yaw");
+  }
+  if (result===0 && gender= 1){
+        alert("You were born on a SUNDAY  and your name would be KWASI");
+    }*
+  else {
+    document.write(result);
+  }*/
 
 
 
